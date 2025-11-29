9443eeeab856913ee25b240527abed4827dbba9e
rxdrx <rodrisisko@gmail.com>
2025-11-29 13:38:56 -0300

    datos mockeados

---
+++ src/mocks/categorias.mock.js
@@ -0,0 +1,29 @@
+const categoriasMock = [
+  { 
+    id_categoria: 1, 
+    nombre: "Zapatillas deportivas", 
+    descripcion: "Calzado para actividades deportivas y running" 
+  },
+  { 
+    id_categoria: 2, 
+    nombre: "Zapatos formales", 
+    descripcion: "Calzado elegante para eventos formales" 
+  },
+  { 
+    id_categoria: 3, 
+    nombre: "Botas", 
+    descripcion: "Botas para diferentes ocasiones y climas" 
+  },
+  { 
+    id_categoria: 4, 
+    nombre: "Sandalias", 
+    descripcion: "Calzado fresco para verano y playa" 
+  },
+  { 
+    id_categoria: 5, 
+    nombre: "Zapatillas urbanas", 
+    descripcion: "Calzado casual para el día a día" 
+  }
+];
+
+module.exports = categoriasMock;
