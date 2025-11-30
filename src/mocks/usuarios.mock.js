20a3148d6d8c7347548a826bc89522290c5c7128
Simón Bucci <sbucci.cnsl@gmail.com>
2025-11-30 01:11:17 -0300

    KAN22_ESLint_config

--- src/mocks/usuarios.mock.js
+++ src/mocks/usuarios.mock.js
@@ -1,23 +1,23 @@
 const usuariosMock = [
   {
     id_usuario: 1,
-    correo: "cliente@ejemplo.com",
-    contrasena: "cliente123",
-    nombre: "Juan",
-    apellido: "Pérez",
-    telefono: "2915555555",
-    rol: "cliente",
-    fecha_creacion: "2024-01-15T10:30:00Z"
+    correo: 'cliente@ejemplo.com',
+    contrasena: 'cliente123',
+    nombre: 'Juan',
+    apellido: 'Pérez',
+    telefono: '2915555555',
+    rol: 'cliente',
+    fecha_creacion: '2024-01-15T10:30:00Z'
   },
   {
     id_usuario: 2,
-    correo: "admin@ejemplo.com",
-    contrasena: "admin123",
-    nombre: "Admin",
-    apellido: "Sistema",
-    telefono: "2914444444",
-    rol: "administrador",
-    fecha_creacion: "2024-01-01T08:00:00Z"
+    correo: 'admin@ejemplo.com',
+    contrasena: 'admin123',
+    nombre: 'Admin',
+    apellido: 'Sistema',
+    telefono: '2914444444',
+    rol: 'administrador',
+    fecha_creacion: '2024-01-01T08:00:00Z'
   }
 ];
 
